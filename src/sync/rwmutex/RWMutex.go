9ff647d245863b1bb5d5079742040c4aad51f93e
Abhay Bothra <bothra90@users.noreply.github.com>
2018-03-05 15:24:48 -0800

    Make default RWMutex non-starving
    In addition, added an implementation for Writer Priority Reader-Writer
    lists, which give priority to writers over readers.
    Note that the later might starve readers, just like the original
    implementation could starve writers.

--- src/sync/rwmutex/RWMutex.go
+++ src/sync/rwmutex/RWMutex.go
@@ -5,6 +5,15 @@ import (
 	"sync/mutex"
 )
 
+// Implementaton notes:
+// We implement the RWMutex using a resource_mutex, and a lightswitch to share
+// access to the "resource" b/w readers. A writer will directly lock the
+// resource_mutex, preventing other readers and writers from locking it.
+// Readers, on the other hand, share access to the resource using a lightswitch
+// which in turn locks and unlocks the resource when the first reader enters and
+// last reader leaves respectively. In addition, we use a "queue" mutex to
+// prevent starvation.
+
 type RWMutex interface {
 	Lock()
 	Unlock()
@@ -15,17 +24,14 @@ type RWMutex interface {
 type rwmutex struct {
 	resource_mutex mutex.Mutex
 	readers_switch lightswitch.LightSwitch
-}
-
-func New() *rwmutex {
-	mu := mutex.New()
-	return &rwmutex{
-		resource_mutex: mu,
-		readers_switch: lightswitch.New(mu),
-	}
+	queue          mutex.Mutex
 }
 
 func (rw *rwmutex) Lock() {
+	// Lock the queue. This queues any incoming readers and writers after this
+	// writer to block behind this writer.
+	rw.queue.Lock()
+	defer rw.queue.Unlock()
 	// Lock the resource directly.
 	rw.resource_mutex.Lock()
 }
@@ -36,11 +42,65 @@ func (rw *rwmutex) Unlock() {
 }
 
 func (rw *rwmutex) RLock() {
-	// Lock resource via lightswithc.
+	// Lock the queue. This forces readers to queue behind other readers and
+	// writers. If no writer is queued, the readers should be able to proceed one-
+	// by-one into the readers_switch.Lock().
+	rw.queue.Lock()
+	defer rw.queue.Unlock()
+	// Lock resource via lightswitch.
 	rw.readers_switch.Lock()
 }
 
 func (rw *rwmutex) RUnlock() {
-	// Unlock resource via lightswithc.
+	// Unlock resource via lightswitch.
 	rw.readers_switch.Unlock()
 }
+
+// A writerPriorityRWMutex gives priority to writers over readers by allowing
+// any waiting writer to proceed before any waiting reader.
+type writerPriorityRWMutex struct {
+	noReaders   mutex.Mutex
+	readSwitch  lightswitch.LightSwitch // Controls noWriters.
+	noWriters   mutex.Mutex
+	writeSwitch lightswitch.LightSwitch // Controls noReaders.
+}
+
+func (rw *writerPriorityRWMutex) Lock() {
+	rw.writeSwitch.Lock()
+	rw.noWriters.Lock()
+}
+
+func (rw *writerPriorityRWMutex) Unlock() {
+	rw.noWriters.Unlock()
+	rw.writeSwitch.Unlock()
+}
+
+func (rw *writerPriorityRWMutex) RLock() {
+	rw.noReaders.Lock()
+	defer rw.noReaders.Unlock()
+	rw.readSwitch.Lock()
+}
+
+func (rw *writerPriorityRWMutex) RUnlock() {
+	rw.readSwitch.Unlock()
+}
+
+func New() *rwmutex {
+	mu := mutex.New()
+	return &rwmutex{
+		resource_mutex: mu,
+		readers_switch: lightswitch.New(mu),
+		queue:          mutex.New(),
+	}
+}
+
+//func New() RWMutex {
+//readMutex := mutex.New()
+//writeMutex := mutex.New()
+//return &writerPriorityRWMutex{
+//noReaders:   readMutex,
+//noWriters:   writeMutex,
+//readSwitch:  lightswitch.New(writeMutex),
+//writeSwitch: lightswitch.New(readMutex),
+//}
+//}
